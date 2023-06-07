c3eeba6d9a4d872f2ef8199da86c9a848f065230
Alex Balaboshko <alex.balaboshko@gmail.com>
2023-06-07 22:41:24 +0300

    Add smart-resize/smart-splits (dont know why)
    Signed-off-by: Alex Balaboshko <alex.balaboshko@gmail.com>

---
+++ lua/plugins/smart-resize.lua
@@ -0,0 +1,8 @@
+return {
+'mrjones2014/smart-splits.nvim',
+    config = function()
+        require('shade').setup({
+            overlay_opacity = 30,
+        })
+    end,
+}
---
+++ lua/plugins/smart-splits.lua
@@ -0,0 +1,13 @@
+return {
+    'mrjones2014/smart-splits.nvim',
+    config = function()
+        require('smart-splits').setup({
+            -- Ignored filetypes (only while resizing)
+            ignored_filetypes = {
+                'nofile',
+                'quickfix',
+                'prompt',
+            },
+        })
+    end,
+}
