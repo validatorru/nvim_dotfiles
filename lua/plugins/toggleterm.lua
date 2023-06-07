428663e45f1b595e2e75dd583bcdf7ef680d2be6
Alex Balaboshko <alex.balaboshko@gmail.com>
2023-06-07 15:56:53 +0300

    Set up toggle term
    Signed-off-by: Alex Balaboshko <alex.balaboshko@gmail.com>

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,40 @@
+function _G.set_terminal_keymaps()
+  local opts = {buffer = 0}
+  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+  vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+end
+
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+return {
+    'akinsho/toggleterm.nvim',
+    version = "*",
+    opts = {},
+    config = function()
+        require("toggleterm").setup({
+            direction = 'float',
+            shade_terminals = true,
+            shading_factor = '80',
+            hide_numbers = true,
+            float_opts = {
+                border = 'curved',
+            }
+        })
+        vim.keymap.set(
+            'n',
+            '<C-t>',
+            '<Cmd>exe v:count1 . "ToggleTerm direction=float"<CR>',
+            {silent = true}
+        )
+        vim.keymap.set(
+            'n',
+            '<S-t>',
+            '<Cmd>exe v:count1 . "ToggleTerm direction=horizontal"<CR>',
+            {silent = true}
+        )
+    end,
+}
