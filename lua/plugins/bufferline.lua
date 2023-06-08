fa5bd1f4bc39a68e71cfbf8a8bf53952350f8c21
Alex Balaboshko <alex.balaboshko@gmail.com>
2023-06-08 16:05:29 +0300

    Install and set up Bufefrline
    Signed-off-by: Alex Balaboshko <alex.balaboshko@gmail.com>

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,21 @@
+return {
+    'akinsho/bufferline.nvim',
+    version = "*",
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    config = function()
+        vim.opt.termguicolors = true
+        local bufferline = require('bufferline')
+        bufferline.setup{
+            options = {
+                style_preset = bufferline.style_preset.minimal, -- or bufferline.style_preset.minimal,
+                mode = "tabs", -- set to "tabs" to only show tabpages instead
+                separator_style = 'thick',
+                themable = true, -- allows highlight groups to be overriden i.e. sets highlights as default
+                -- indicator = {
+                --     icon = '▎', -- this should be omitted if indicator style is not 'icon'
+                --     style = 'underline', -- | 'underline' | 'none',
+                -- },
+            }
+        }
+    end,
+}
