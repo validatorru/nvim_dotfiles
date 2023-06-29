e4834029aa79277f9c6f98024e80873326d4047b
Alex Balaboshko <alex.balaboshko@gmail.com>
2023-06-29 20:26:31 +0300

    Add Telescope extensions and keymaps
    * Tele_tabby
    * Telscope Tmux
    * Add keymaps for opening these extensions and media_files
    
    Signed-off-by: Alex Balaboshko <alex.balaboshko@gmail.com>

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -16,14 +16,27 @@ return {
         require('telescope').load_extension('media_files')
         require('telescope').load_extension('http')
         require('telescope').load_extension('coc')
+        require('telescope').load_extension('tmux')
+        require('telescope').load_extension('tele_tabby')
+        -- Open find files 
         vim.keymap.set('n', ',ff', builtin.find_files, {})
+        -- Open text search
         vim.keymap.set('n', ',fg', builtin.live_grep, {})
+        --  Open buffers list
         vim.keymap.set('n', ',fb', builtin.buffers, {})
+        -- Open tabs list
+        vim.keymap.set('n', ',ft', ':Telescope tele_tabby list <CR>', {})
+        -- Open media files
+        vim.keymap.set('n', ',fm', ':Telescope media_files <CR>', {})
+        -- Open media files
+        vim.keymap.set('n', ',fs', ':Telescope tmux sessions <CR>', {})
 
     end,
     dependencies = {
         {'nvim-lua/plenary.nvim'},
         {'nvim-lua/popup.nvim'},
+        {'camgraff/telescope-tmux.nvim'},
+        {'TC72/telescope-tele-tabby.nvim'},
         {'fannheyward/telescope-coc.nvim'},
         {'barrett-ruth/telescope-http.nvim'},
         {'nvim-telescope/telescope-media-files.nvim'},
