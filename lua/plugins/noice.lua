d0128b50bfbe3dc0f84fa7639ea2da8616e76c31
Alex Balaboshko <alex.balaboshko@gmail.com>
2024-05-24 03:29:39 +0300

    Reconfigure Noice to use borderless command pallete and mini notifier
    Signed-off-by: Alex Balaboshko <alex.balaboshko@gmail.com>

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -5,19 +5,56 @@ return {
     -- add any options here
   },
   dependencies = {
-    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
     "MunifTanjim/nui.nvim",
-    -- OPTIONAL:
-    --   `nvim-notify` is only needed, if you want to use the notification view.
-    --   If not available, we use `mini` as the fallback
     "rcarriga/nvim-notify",
+    "hrsh7th/nvim-cmp",
   },
 
 
   config = function()
     require("noice").setup({
+      views = {
+        cmdline_popup = {
+          position = {
+            row = 5,
+            col = "50%",
+          },
+          size = {
+            width = 60,
+            height = "auto",
+          },
+          border = {
+            style = "none",
+            padding = { 2, 2 },
+          },
+          win_options = {
+            winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
+          },
+        },
+        popupmenu = {
+          relative = "editor",
+          position = {
+            row = 10,
+            col = "50%",
+          },
+          size = {
+            width = 60,
+            height = 14,
+          },
+          border = {
+            style = "none",
+            padding = { 2, 2 },
+          },
+          win_options = {
+            winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
+          },
+        },
+      },
+      messages = {
+        view = "mini",
+        enter = true,
+      },
       lsp = {
-        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
         override = {
           ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
           ["vim.lsp.util.stylize_markdown"] = true,
@@ -26,8 +63,8 @@ return {
       },
       -- you can enable a preset for easier configuration
       presets = {
-        bottom_search = true, -- use a classic bottom cmdline for search
-        command_palette = true, -- position the cmdline and popupmenu together
+        -- bottom_search = true, -- use a classic bottom cmdline for search
+        -- command_palette = true, -- position the cmdline and popupmenu together
         long_message_to_split = true, -- long messages will be sent to a split
         inc_rename = false, -- enables an input dialog for inc-rename.nvim
         lsp_doc_border = false, -- add a border to hover docs and signature help
