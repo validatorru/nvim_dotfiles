a9ee0eb87082a0610c95c2c02e6fd1a310ab680c
Alex Balaboshko <alex.balaboshko@gmail.com>
2025-04-02 16:37:18 +0300

    Update: change noice settings, change telescope codestyle
    Signed-off-by: Alex Balaboshko <alex.balaboshko@gmail.com>

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,72 +1,74 @@
 return {
-  "folke/noice.nvim",
-  event = "VeryLazy",
-  opts = {
-    -- add any options here
-  },
-  dependencies = {
-    "MunifTanjim/nui.nvim",
-    "rcarriga/nvim-notify",
-    "hrsh7th/nvim-cmp",
-  },
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+        -- add any options here
+    },
+    dependencies = {
+        "MunifTanjim/nui.nvim",
+        "rcarriga/nvim-notify",
+        "hrsh7th/nvim-cmp",
+    },
 
 
-  config = function()
-    require("noice").setup({
-      views = {
-        cmdline_popup = {
-          position = {
-            row = "50%",
-            col = "50%",
-          },
-          size = {
-            width = "50%",
-            height = "auto",
-          },
-          border = {
-            style = "rounded",
-            padding = { 2, 2 },
-          },
-          win_options = {
-            winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
-          },
-        },
-        popupmenu = {
-          relative = "editor",
-          position = {
-            row = 10,
-            col = "50%",
-          },
-          size = {
-            width = 60,
-            height = 14,
-          },
-          border = {
-            style = "none",
-            padding = { 2, 2 },
-          },
-          win_options = {
-            winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
-          },
-        },
-      },
-      messages = {
-      },
-      lsp = {
-        override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          ["vim.lsp.util.stylize_markdown"] = true,
-          ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-        },
-      },
-      -- you can enable a preset for easier configuration
-      presets = {
-        -- bottom_search = true, -- use a classic bottom cmdline for search
-        command_palette = true, -- position the cmdline and popupmenu together
-        long_message_to_split = true, -- long messages will be sent to a split
-        inc_rename = false, -- enables an input dialog for inc-rename.nvim
-        lsp_doc_border = true, -- add a border to hover docs and signature help
-      },
-    })
-  end,
+    config = function()
+        require("noice").setup({
+            views = {
+                cmdline_popup = {
+                    position = {
+                        row = 15,
+                        col = "50%",
+                    },
+                    size = {
+                        width = "50%",
+                        height = "auto",
+                    },
+                    border = {
+                        style = "rounded",
+                        padding = { 2, 2 },
+                    },
+                    win_options = {
+                        winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
+                    },
+                },
+                popupmenu = {
+                    enabled = true,
+                    relative = "editor",
+                    backend = "nui",
+                    position = {
+                        row = 18,
+                        col = "50%",
+                    },
+                    size = {
+                        width = '50%',
+                        height = 10,
+                    },
+                    border = {
+                        style = "none",
+                        padding = { 4, 2 },
+                    },
+                    win_options = {
+                        winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
+                    },
+                },
+            },
+            messages = {
+            },
+            lsp = {
+                override = {
+                    ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                    ["vim.lsp.util.stylize_markdown"] = true,
+                    ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+                },
+            },
+            -- you can enable a preset for easier configuration
+            presets = {
+                -- bottom_search = true, -- use a classic bottom cmdline for search
+                -- command_palette = true, -- position the cmdline and popupmenu together
+                long_message_to_split = true, -- long messages will be sent to a split
+                inc_rename = false, -- enables an input dialog for inc-rename.nvim
+                lsp_doc_border = true, -- add a border to hover docs and signature help
+            },
+        })
+    end,
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,67 +1,74 @@
 return {
-  'nvim-telescope/telescope.nvim', tag = '0.1.5',
-  config = function()
-    require('telescope').setup({
-      defaults = {
-        file_ignore_patterns = {
-          "node_modules", "public", "vendor", "*.min.*"
-        },
-      },
-      extensions = {
-        media_files = {
-          -- filetypes whitelist
-          -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
-          filetypes = {"png", "webp", "jpg", "jpeg"},
-          -- find command (defaults to `fd`)
-          find_cmd = "rg"
-        },
-        aerial = {
-          -- Display symbols as <root>.<parent>.<symbol>
-          show_nesting = {
-            ['_'] = false, -- This key will be the default
-            json = true,   -- You can set the option for specific filetypes
-            yaml = true,
-          }
-        },
-        }
-      })
-    local builtin = require('telescope.builtin')
-    require('telescope').load_extension('media_files')
-    -- require('telescope').load_extension('http')
-    require('telescope').load_extension('coc')
-    require('telescope').load_extension('telescope-tabs')
-    require('telescope').load_extension('env')
-    require('telescope').load_extension('aerial')
-    require("telescope").load_extension("noice")
-    -- Open find files 
-    vim.keymap.set('n', ',ff', builtin.find_files, {})
-    -- Open text search
-    vim.keymap.set('n', ',fg', builtin.live_grep, {})
-    vim.keymap.set('n', ',fG', builtin.grep_string, {})
-    --  Open buffers list
-    vim.keymap.set('n', ',fb', builtin.buffers, {})
-    -- Open tabs list
-    vim.keymap.set('n', ',ft', ':Telescope telescope-tabs list_tabs <CR>', {})
-    -- Open media files
-    vim.keymap.set('n', ',fm', ':Telescope media_files <CR>', {})
-    -- Open symbols list
-    vim.keymap.set('n', ',fa', ':Telescope aerial <CR>', {})
-    -- vim.keymap.set('n', ',fa', ':Telescope aerial <CR>', {})
+    'nvim-telescope/telescope.nvim', tag = '0.1.5',
+    config = function()
+        require('telescope').setup({
+            defaults = {
+                file_ignore_patterns = {
+                    "node_modules", "public", "vendor", "*.min.*"
+                },
+            },
+            extensions = {
+                media_files = {
+                    -- filetypes whitelist
+                    -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
+                    filetypes = {"png", "webp", "jpg", "jpeg"},
+                    -- find command (defaults to `fd`)
+                    find_cmd = "rg"
+                },
+                aerial = {
+                    -- Display symbols as <root>.<parent>.<symbol>
+                    show_nesting = {
+                        ['_'] = false, -- This key will be the default
+                        json = true,   -- You can set the option for specific filetypes
+                        yaml = true,
+                    }
+                },
+            },
+        })
+        local builtin = require('telescope.builtin')
+        require('telescope').load_extension('media_files')
+        -- require('telescope').load_extension('http')
+        require('telescope').load_extension('coc')
+        require('telescope').load_extension('telescope-tabs')
+        require('telescope').load_extension('env')
+        require('telescope').load_extension('aerial')
+        require("telescope").load_extension("noice")
+        require('telescope').load_extension('git_diffs')
+        -- Open find files 
+        vim.keymap.set('n', ',ff', builtin.find_files, {})
+        -- Open text search
+        vim.keymap.set('n', ',fg', builtin.live_grep, {})
+        vim.keymap.set('n', ',fG', builtin.grep_string, {})
+        --  Open buffers list
+        vim.keymap.set('n', ',fb', builtin.buffers, {})
+        -- Open tabs list
+        vim.keymap.set('n', ',ft', ':Telescope telescope-tabs list_tabs <CR>', {})
+        -- Open media files
+        vim.keymap.set('n', ',fm', ':Telescope media_files <CR>', {})
+        -- Open symbols list
+        vim.keymap.set('n', ',fa', ':Telescope aerial <CR>', {})
+        -- vim.keymap.set('n', ',fa', ':Telescope aerial <CR>', {})
 
-  end,
-  dependencies = {
-    {'nvim-lua/plenary.nvim'},
-    {'nvim-lua/popup.nvim'},
-    {'LinArcX/telescope-env.nvim'},
-    {'LukasPietzschmann/telescope-tabs'},
-    {'fannheyward/telescope-coc.nvim'},
-    -- {'barrett-ruth/telescope-http.nvim'},
-    {'nvim-telescope/telescope-media-files.nvim'},
-    {"nvim-telescope/telescope-fzf-native.nvim",
-      build = "make",
-      config = function()
-        require("telescope").load_extension("fzf")
-      end,
-    }
-  },
+    end,
+    dependencies = {
+        {'nvim-lua/plenary.nvim'},
+        {'nvim-lua/popup.nvim'},
+        {'LinArcX/telescope-env.nvim'},
+        {'LukasPietzschmann/telescope-tabs'},
+        {'fannheyward/telescope-coc.nvim'},
+        -- {'barrett-ruth/telescope-http.nvim'},
+        {'nvim-telescope/telescope-media-files.nvim'},
+        {'paopaol/telescope-git-diffs.nvim',
+            dependencies = {
+                "nvim-lua/plenary.nvim",
+                "sindrets/diffview.nvim",
+            },
+        },
+        {"nvim-telescope/telescope-fzf-native.nvim",
+            build = "make",
+            config = function()
+                require("telescope").load_extension("fzf")
+            end,
+        }
+    },
 }
