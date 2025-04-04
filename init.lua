56131ea2554679ff6f2a0cc8eb3215e87df81f8d
Alex Balaboshko <alex.balaboshko@gmail.com>
2025-04-04 15:56:38 +0300

    Update: set up viretual text blocks
    Signed-off-by: Alex Balaboshko <alex.balaboshko@gmail.com>

--- init.lua
+++ init.lua
@@ -151,3 +151,16 @@ vim.cmd[[
 ]]
 
 vim.opt.virtualedit = "block"
+
+vim.diagnostic.config({
+  -- Use the default configuration
+  virtual_lines = true
+
+  -- Alternatively, customize specific options
+  -- virtual_lines = {
+  --  -- Only show virtual line diagnostics for the current cursor line
+  --  current_line = true,
+  -- },
+})
+vim.g.loaded_perl_provider = 0
+require('render-markdown').setup({ latex = { enabled = false } })
