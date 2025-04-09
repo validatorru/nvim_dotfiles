a1fab7e592ebeff0485888b80543a66b51305dd9
Alex Balaboshko <alex.balaboshko@gmail.com>
2025-04-09 17:48:34 +0300

    Update: install todo comments
    Signed-off-by: Alex Balaboshko <alex.balaboshko@gmail.com>

---
+++ lua/plugins/todo-comments.lua
@@ -0,0 +1,29 @@
+return {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+        FIX = {
+            alt = {
+                "fixme",
+                "bug",
+                "fixit",
+                "issue",
+                "Fixme",
+                "Bug",
+                "Fixit",
+                "Issue"
+            }, 
+        },
+        TODO = {
+            alt = {
+                "Todo",
+                "todo"
+            }, 
+        },
+        HACK = {},
+        WARN = {},
+        PERF = {},
+        NOTE = {},
+        TEST = {},
+    }
+}
