7af6573fa158034621dda59118d9618c7cff6f57
Alex Balaboshko <alex.balaboshko@gmail.com>
2025-04-10 17:12:09 +0300

    Update: tweak Avante config, change keybindings
    Signed-off-by: Alex Balaboshko <alex.balaboshko@gmail.com>

--- init.lua
+++ init.lua
@@ -1,14 +1,14 @@
 -- Enable lazy nvim plugin manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -35,7 +35,7 @@ vim.api.nvim_set_hl(0, 'CursorLine', { bg = '#3c3836' })
 vim.api.nvim_set_hl(0, 'WinSeparator', { fg = '#000000' })
 vim.o.cursorlineopt ='both'
 vim.api.nvim_set_option_value("termguicolors", true, {
-  scope = "local",
+    scope = "local",
 })
 -- vim.api.nvim_set_option_value("colorcolumn", "79", {
 --   scope = "local",
@@ -92,8 +92,8 @@ vim.keymap.set('n', ',C', ':set cc=79<CR>')
 -- })
 
 vim.api.nvim_create_autocmd({"BufNewFile", "BufRead", "BufEnter", "BufWinEnter", }, {
-  pattern = {"*.blade.php"},
-  command = "set filetype=blade",
+    pattern = {"*.blade.php"},
+    command = "set filetype=blade",
 })
 
 -- augroup BladeFiltypeRelated
@@ -119,37 +119,40 @@ vim.keymap.set('ia', 'importhm', "from helpers.logging_helper import hm_log<ESC>
 -- blade files treesitter
 local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
 parser_config.blade = {
-  install_info = {
-    url = "https://github.com/EmranMR/tree-sitter-blade",
-    files = {"src/parser.c"},
-    branch = "main",
-  },
-  filetype = "blade"
+    install_info = {
+        url = "https://github.com/EmranMR/tree-sitter-blade",
+        files = {"src/parser.c"},
+        branch = "main",
+    },
+    filetype = "blade"
 }
 
 local augroup = vim.api.nvim_create_augroup   -- Create/get autocommand group
 local autocmd = vim.api.nvim_create_autocmd
 
 vim.filetype.add({
-  pattern = {
-    ['.*%.blade%.php'] = 'blade',
-  },
+    pattern = {
+        ['.*%.blade%.php'] = 'blade',
+    },
 })
 
 -- highlight yanked text for 200ms using the "Visual" highlight group
 vim.cmd[[
-  augroup highlight_yank
-  autocmd!
-  au TextYankPost * silent! lua vim.highlight.on_yank({higroup="Visual", timeout=200})
-  augroup END
+        augroup highlight_yank
+        autocmd!
+        au TextYankPost * silent! lua vim.highlight.on_yank({higroup="Visual", timeout=200})
+        augroup END
 ]]
 
 vim.opt.virtualedit = "block"
 
 vim.diagnostic.config({
-  virtual_lines = true
+    virtual_lines = true
 })
 
+-- views can only be fully collapsed with the global statusline
+vim.opt.laststatus = 3
+vim.opt.timeoutlen = 2000
 vim.g.loaded_perl_provider = 0
 require('render-markdown').setup({ latex = { enabled = false } })
 vim.api.nvim_set_hl(0, 'ColorColumn', { bg = none, fg = '#612f33' })
--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -11,50 +11,77 @@ return {
                 api_key_name = "OPENAI_API_KEY",
                 endpoint = "https://api.deepseek.com",
                 model = "deepseek-coder",
-                --   model = "deepseek-chat", -- your desired model (or use gpt-4o, etc.)
+                -- model = "deepseek-chat", -- your desired model (or use gpt-4o, etc.)
                 max_tokens = 8192,
             },
+            openai = {
+                api_key_name = "OPENAI_API_KEY",
+                model = "gpt-4",
+                max_tokens = 4096,
+            },
+            copilot = {
+                api_key_name = "COPILOT_API_KEY",
+                model = "copilot",
+            },
+        },
+        keybindings = {
+            toggle = ",aa", -- Toggle Avante UI (changed from `<leader>aa`)
+            -- ask = ",aq",    -- Ask Avante a question (unchanged)
+            insert = ",ai", -- Insert AI-generated text (changed from `<leader>ai`)
         },
     },
-    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
     build = "make",
-    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
     dependencies = {
         "nvim-treesitter/nvim-treesitter",
         "stevearc/dressing.nvim",
         "nvim-lua/plenary.nvim",
         "MunifTanjim/nui.nvim",
-        --- The below dependencies are optional,
+
         "echasnovski/mini.pick", -- for file_selector provider mini.pick
         "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
         "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
         "ibhagwan/fzf-lua", -- for file_selector provider fzf
         "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-        -- "zbirenbaum/copilot.lua", -- for providers='copilot'
+        "zbirenbaum/copilot.lua", -- for providers='copilot'
         {
-            -- support for image pasting
             "HakonHarnes/img-clip.nvim",
             event = "VeryLazy",
             opts = {
-                -- recommended settings
                 default = {
-                    embed_image_as_base64 = false,
+                    embed_image_as_base64 = true, -- Enable base64 embedding for better compatibility
                     prompt_for_file_name = false,
                     drag_and_drop = {
                         insert_mode = true,
                     },
-                    -- required for Windows users
+
                     use_absolute_path = true,
                 },
             },
         },
         {
-            -- Make sure to set this up properly if you have lazy=true
+
             'MeanderingProgrammer/render-markdown.nvim',
             opts = {
                 file_types = { "markdown", "Avante" },
+                render = {
+                    highlight = true, -- Enable syntax highlighting
+                    toc = true,       -- Enable table of contents
+                },
             },
             ft = { "markdown", "Avante" },
         },
     },
+    config = function(_, opts)
+        require("avante").setup(opts)
+        -- Additional setup for keybindings
+        vim.keymap.set("n", opts.keybindings.toggle, function()
+            require("avante").toggle()
+        end, { desc = "Toggle Avante UI" })
+        vim.keymap.set("n", opts.keybindings.ask, function()
+            require("avante").ask()
+        end, { desc = "Ask Avante a question" })
+        vim.keymap.set("n", opts.keybindings.insert, function()
+            require("avante").insert()
+        end, { desc = "Insert AI-generated text" })
+    end,
 }
