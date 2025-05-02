695b99f647430c8c0dca0b53080f58418a337f11
Alex Balaboshko <alex.balaboshko@gmail.com>
2025-05-02 18:18:33 +0300

    Update: change noice settings
    Signed-off-by: Alex Balaboshko <alex.balaboshko@gmail.com>

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -53,6 +53,18 @@ return {
                 },
             },
             messages = {
+                -- enabled = true, -- enables the Noice messages UI
+                -- view = "notify", -- default view for messages
+                -- view_error = "notify", -- view for errors
+                -- view_warn = "notify", -- view for warnings
+                -- view_history = "messages", -- view for :messages
+                -- view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
+                enabled = true, -- enables the Noice messages UI
+                -- view = "messages", -- default view for messages
+                -- view_error = "messages", -- view for errors
+                -- view_warn = "messages", -- view for warnings
+                -- view_history = "messages", -- view for :messages
+                -- view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
             },
             lsp = {
                 override = {
