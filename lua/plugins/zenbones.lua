1eb33599f23cb5d9ea8f037c53258e2132934908
Alex Balaboshko <alex.balaboshko@gmail.com>
2025-06-10 16:57:06 +0300

    Update: do not enable custom colortheme on linux
    Signed-off-by: Alex Balaboshko <alex.balaboshko@gmail.com>

--- lua/plugins/zenbones.lua
+++ lua/plugins/zenbones.lua
@@ -3,6 +3,7 @@ return {
     dependencies = "rktjmp/lush.nvim",
     lazy = false,
     priority = 1000,
+    enabled = (jit.os ~= "Linux"),
     -- you can set set configuration options here
     config = function()
         vim.cmd.colorscheme('kanagawabones')
