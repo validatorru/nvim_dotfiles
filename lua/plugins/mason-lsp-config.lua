d9855efdeec0e65cd2e69f55998e195a5e2cbe2e
Alex Balaboshko <alex.balaboshko@gmail.com>
2025-08-28 15:36:51 +0300

    Update: configure lsps
    Signed-off-by: Alex Balaboshko <alex.balaboshko@gmail.com>

---
+++ lua/plugins/mason-lsp-config.lua
@@ -0,0 +1,13 @@
+return {
+    "mason-org/mason-lspconfig.nvim",
+    opts = {},
+    dependencies = {
+        { "mason-org/mason.nvim", opts = {} },
+        "neovim/nvim-lspconfig",
+    },
+    config = function()
+        require("mason-lspconfig").setup {
+            automatic_enable = false,
+        }
+    end
+}
