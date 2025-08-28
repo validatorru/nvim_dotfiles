44eaaed1060016b9e1fb3cda22e4c54cd47eecdc
Alex Balaboshko <alex.balaboshko@gmail.com>
2025-08-28 15:37:20 +0300

    Update: configure lsp hovers and Noice UI
    Signed-off-by: Alex Balaboshko <alex.balaboshko@gmail.com>

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -63,7 +63,6 @@ return {
                 view_error = "messages", -- view for errors
                 view_warn = "notify", -- view for warnings
                 view_history = "messages", -- view for :messages
-                -- view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
             },
             notify = {
                 -- Noice can be used as `vim.notify` so you can route any notification like other messages
@@ -90,32 +89,58 @@ return {
                     view = "mini",
                 },
                 override = {
-                    ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-                    ["vim.lsp.util.stylize_markdown"] = true,
-                    ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+                    -- override the default lsp markdown formatter with Noice
+                    ["vim.lsp.util.convert_input_to_markdown_lines"] = false,
+                    -- override the lsp markdown formatter with Noice
+                    ["vim.lsp.util.stylize_markdown"] = false,
+                    -- override cmp documentation with Noice (needs the other options to work)
+                    ["cmp.entry.get_documentation"] = false,
                 },
-            },
-            hover = {
-                enabled = true,
-                silent = true, -- set to true to not show a message if hover is not available
-                view = nil, -- when nil, use defaults from documentation
-                ---@type NoiceViewOptions
-                opts = {
-                    border = {
-                        style = "none",
-                        padding = { 4, 2 },
+                hover = {
+                    enabled = true,
+                    silent = true, -- set to true to not show a message if hover is not available
+                    view = nil, -- when nil, use defaults from documentation
+                    opts = {
+                        border = {
+                            style = "single",
+                            padding = { 2, 2 },
+
+                        },
+                        win_options = {
+                            winhighlight = "Pmenu:Pmenu,FloatBorder:FloatBorder",
+                        },
+
+                    }, -- merged with defaults from documentation
+                },
+                signature = {
+                    enabled = true,
+                    auto_open = {
+                        enabled = true,
+                        trigger = true, -- Automatically show signature help when typing a trigger character from the LSP
+                        luasnip = true, -- Will open signature help when jumping to Luasnip insert nodes
+                        throttle = 50, -- Debounce lsp signature help request by 50ms
                     },
-                }, -- merged with defaults from documentation
-            },
-            -- defaults for hover and signature help
-            documentation = {
-                view = "hover",
-                opts = {
-                    lang = "markdown",
-                    replace = true,
-                    render = "plain",
-                    format = { "{message}" },
-                    win_options = { concealcursor = "n", conceallevel = 3 },
+                    view = nil, -- when nil, use defaults from documentation
+                    ---@type NoiceViewOptions
+                    opts = {}, -- merged with defaults from documentation
+                },
+                message = {
+                    -- Messages shown by lsp servers
+                    enabled = true,
+                    view = "notify",
+                    opts = {},
+                },
+                -- defaults for hover and signature help
+                documentation = {
+                    view = "hover",
+                    ---@type NoiceViewOptions
+                    opts = {
+                        lang = "markdown",
+                        replace = true,
+                        render = "plain",
+                        format = { "{message}" },
+                        win_options = { concealcursor = "n", conceallevel = 3 },
+                    },
                 },
             },
             -- you can enable a preset for easier configuration
@@ -127,5 +152,16 @@ return {
                 -- lsp_doc_border = true, -- add a border to hover docs and signature help
             },
         })
+        vim.keymap.set({ "n", "i", "s" }, "<c-f>", function()
+            if not require("noice.lsp").scroll(4) then
+                return "<c-f>"
+            end
+        end, { silent = true, expr = true })
+
+        vim.keymap.set({ "n", "i", "s" }, "<c-b>", function()
+            if not require("noice.lsp").scroll(-4) then
+                return "<c-b>"
+            end
+        end, { silent = true, expr = true })
     end,
 }
