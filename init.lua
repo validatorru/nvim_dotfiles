f9cc671becf99d8d2b8f70063b3a656443a8954a
Alex Balaboshko <alex.balaboshko@gmail.com>
2025-11-24 03:55:36 +0300

    Add: scooter integration for find and replace (via snacks)
    Signed-off-by: Alex Balaboshko <alex.balaboshko@gmail.com>

--- init.lua
+++ init.lua
@@ -215,3 +215,65 @@ vim.keymap.set('v', ' s', function()
         start_pos[1]-1, start_pos[2], end_pos[1]-1, end_pos[2], 
         result_lines)
 end, { noremap = true, silent = true, desc = "Strip HTML tags (keep newlines)" })
+
+
+
+
+
+
+local scooter_term = nil
+
+-- Called by scooter to open the selected file at the correct line from the scooter search list
+_G.EditLineFromScooter = function(file_path, line)
+    if scooter_term and scooter_term:buf_valid() then
+        scooter_term:hide()
+    end
+
+    local current_path = vim.fn.expand("%:p")
+    local target_path = vim.fn.fnamemodify(file_path, ":p")
+
+    if current_path ~= target_path then
+        vim.cmd.edit(vim.fn.fnameescape(file_path))
+    end
+
+    vim.api.nvim_win_set_cursor(0, { line, 0 })
+end
+
+local function is_terminal_running(term)
+    if not term or not term:buf_valid() then
+        return false
+    end
+    local channel = vim.fn.getbufvar(term.buf, 'terminal_job_id')
+    return channel and vim.fn.jobwait({channel}, 0)[1] == -1
+end
+
+local function open_scooter()
+    if is_terminal_running(scooter_term) then
+        scooter_term:toggle()
+    else
+        scooter_term = require("snacks").terminal.open("scooter", {
+            win = { position = "float" },
+        })
+    end
+end
+
+local function open_scooter_with_text(search_text)
+    if scooter_term and scooter_term:buf_valid() then
+        scooter_term:close()
+    end
+
+    local escaped_text = vim.fn.shellescape(search_text:gsub("\r?\n", " "))
+    scooter_term = require("snacks").terminal.open("scooter --fixed-strings --search-text " .. escaped_text, {
+        win = { position = "float" },
+    })
+end
+
+vim.keymap.set('n', '<leader>S', open_scooter, { desc = 'Open scooter' })
+vim.keymap.set('v', '<leader>r',
+    function()
+        local selection = vim.fn.getreg('"')
+        vim.cmd('normal! "ay')
+        open_scooter_with_text(vim.fn.getreg('a'))
+        vim.fn.setreg('"', selection)
+    end,
+    { desc = 'Search selected text in scooter' })
